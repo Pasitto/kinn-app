758954290d571c2bdfb71318ed32ffc3a4e87ac2
pasitto <6010110478@psu.ac.th>
2021-05-10 21:49:41 +0700

    add guitar asset

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>Please select</h1>
+            <a href='/guitar'>Go to Guitar</a>
+            <br/>
+            <a href='/Drum'>Go to Drum</a>
         </div>
     )
 }
